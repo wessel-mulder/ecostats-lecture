bba0ba8a57cc938f93e8f949b06fc0e949417d3a
wessel-mulder <wesselmulder00@gmail.com>
2025-03-08 14:37:17 +0100

    making stacks

--- sdm-data.R
+++ sdm-data.R
@@ -4,38 +4,160 @@ library('terra')
 library('dplyr')
 library('sf')
 
-# GETTING DATA -----------------------------------------------------------
-path <- system.file('external',package='sdm')
-lst <- list.files(path=path,pattern='asc$',full.names = T) # list the name of files in the specified pa
-preds <- c(rast(lst))
-plot(preds)
+# SESSION 1 --------------------------------------------------------------------
+### EUROPE SHAPEFILE
+europe <- vect('data/Europe/Europe_merged.shp')
+europe_extent <- ext(europe)
 
-preds[[1]]
+### TEMPERATURE 
+temp_2015 <- rast('data/current/chelsav2/GLOBAL/climatologies/1981-2010/bio/CHELSA_bio1_1981-2010_V.2.1.tif')
+temp_2015 <- crop(temp_2015,europe)
+prec_2015 <- rast('data/current/chelsav2/GLOBAL/climatologies/1981-2010/bio/CHELSA_bio12_1981-2010_V.2.1.tif')
+prec_2015 <- crop(prec_2015,europe)
 
+temp_2015_test <- aggregate(temp_2015,fact = 30)
+prec_2015_test <- aggregate(prec_2015,fact = 30)
 
-# 2015 --------------------------------------------------------------------
+stack <- c(temp_2015_test,prec_2015_test)
+names(stack) <- c('temperature','precipitation')
+writeRaster(stack, "session1_stack.tif", overwrite = TRUE)
 
+test <- rast('session1_stack.tif')
+plot(test)
+
+# SESSION 2 ---------------------------------------------------------------
+environment <- list()
+
+environment[['current']] <- list(
+  # landuse
+  landuse_path = 'data/current/multiple-states_input4MIPs_landState_ScenarioMIP_UofMD-IMAGE-ssp119-2-1-f_gn_2015-2100.nc',
+  year = 2015,
+  #climate
+  climate_path = 'data/current/chelsav2/GLOBAL/climatologies/1981-2010/bio/',
+  
+  #output file 
+  stack_name = 'session2_2015_stack.tif'
+)
+
+environment[['2100_ssp1']] <- list(
+  # landuse
+  landuse_path = 'data/ssp126/multiple-states_input4MIPs_landState_ScenarioMIP_UofMD-IMAGE-ssp119-2-1-f_gn_2015-2100.nc',
+  year = 2100,
+  #climate
+  climate_path = 'data/ssp126/chelsav2/GLOBAL/climatologies/2071-2100/IPSL-CM6A-LR/ssp126/bio/',
+  
+  #output file 
+  stack_name = 'session2_2100_ssp1_stack.tif'
+)
+
+environment[['2100_ssp5']] <- list(
+  # landuse
+  landuse_path = 'data/ssp585/multiple-states_input4MIPs_landState_ScenarioMIP_UofMD-MAGPIE-ssp534-2-1-f_gn_2015-2100.nc',
+  year = 2100,
+  #climate
+  climate_path = 'data/ssp585/chelsav2/GLOBAL/climatologies/2071-2100/IPSL-CM6A-LR/ssp585/bio/',
+  
+  #output file 
+  stack_name = 'session2_2100_ssp5_stack.tif'
+)
+
+### CURRENT (2015)
+# elevation
 elev <- rast('data/ETOPO_2022_v1_60s_N90W180_bed.tif')
 elev <- crop(elev,europe)
 plot(elev)
 
-### EUROPE SHAPEFILE
-europe <- vect('data/Europe/Europe_merged.shp')
-
-### LANDUSE
+# landuse 
 landuse_ssp1 <- rast('data/current/multiple-states_input4MIPs_landState_ScenarioMIP_UofMD-IMAGE-ssp119-2-1-f_gn_2015-2100.nc')
 time_info <- time(landuse_ssp1)
-
-# 2015
 index <- which(time_info == '2015')
 landuse_2015 <- subset(landuse_ssp1, index)
 landuse_2015 <- crop(landuse_2015,europe)
 landuse_2015 <- landuse_2015[[1:12]]
-dominant_landuse <- which.max(landuse_2015)
-names(landuse_2015)
-plot(dominant_landuse)
-
 plot(landuse_2015)
+dominant_landuse <- which.max(landuse_2015)
+dominant_landuse <- as.factor(dominant_landuse)
+
+reclass_matrix <- matrix(c(
+  # Forests (primary)
+  1, 1,
+  
+  # Forests (secondary)
+  3, 2,
+  
+  # Vegetated lands (primary)
+  2, 3,
+  
+  # Vegetated lands (secondary)
+  4, 4,
+  
+  # managed pastures / rangelands
+  5, 5,
+  6, 5,
+  
+  # urban land
+  7, 7,
+
+  # crop lands 
+  8, 6,
+  9, 6,
+  10, 6,
+  11, 6, 
+  12, 6,
+  
+  # water
+  NA, 8
+  
+), ncol = 2, byrow = TRUE)
+landuse_processed <- classify(dominant_landuse,reclass_matrix)
+levels(landuse_processed) <- data.frame(ID = seq_along(1:8), Class = c('Forests (primary)','Forests (secondary)',
+                                                          'Vegetated lands (primary)','Vegetated lands (secondary)',
+                                                          'Pastures / Rangelands','Croplands','Urban','Water'))
+colors <- c("#11573d",  # 1 forest primary (green)
+            "#329470",  # 1 forest secondary (green)
+            "#7DD181",  # 2 vegetated lands primary (lightgreen)
+            "#7de88f",  # 2 vegetated lands secondary (lightgreen)
+            "#DD7230",  # 3 pasture/rangeland (orange)
+            "#FCE762",  # 4 cropland (yellow)
+            "#D00000",  # 5 urban (red)
+            "#A7C7FF")  # 6 water (blue)
+plot(landuse_processed,
+     col = colors,
+     legend = 'top')
+
+### TEMPERATURE 
+# List all raster files in the folder
+folder_path <- 'data/current/chelsav2/GLOBAL/climatologies/1981-2010/bio/'
+raster_files <- list.files(folder_path, pattern = "\\.tif$", full.names = TRUE)
+
+# Load all raster files into a SpatRaster stack
+raster_stack <- rast(raster_files)
+raster_stack <- crop(raster_stack,europe)
+
+#aggregates
+elev <- aggregate(elev,fact = 15)
+raster_stack <- aggregate(raster_stack,fact = 30)
+
+elev <- project(elev,raster_stack)
+landuse_processed <- project(landuse_processed,raster_stack)
+
+plot(elev)
+plot(landuse_processed)
+plot(raster_stack[[1]])
+
+stack <- c(elev,landuse_processed,raster_stack)
+plot(stack)
+names(stack) <- c('elevation','landuse',
+                  'bio1','bio10','bio11','bio12',
+                  'bio13','bio14','bio15','bio16','bio17',
+                  'bio18','bio19','bio2','bio3','bio4',
+                  'bio5','bio6','bio7','bio8','bio9')
+writeRaster(stack, "session2_2015_stack.tif", overwrite = TRUE)
+
+
+
+
+
 # 2100 ssp1
 index <- which(time_info == '2100')
 landuse_2100_ssp1 <- subset(landuse_ssp1, index)
@@ -58,33 +180,7 @@ plot(landuse_2015[[1]])
 plot(landuse_2100_ssp1[[1]])
 plot(landuse_2100_ssp5[[1]])
 
-### TEMPERATURE 
-temp_2015 <- rast('data/current/chelsav2/GLOBAL/climatologies/1981-2010/bio/CHELSA_bio1_1981-2010_V.2.1.tif')
-temp_2015 <- crop(temp_2015,europe)
-prec_2015 <- rast('data/current/chelsav2/GLOBAL/climatologies/1981-2010/bio/CHELSA_bio12_1981-2010_V.2.1.tif')
-prec_2015 <- crop(prec_2015,europe)
 
-temp_2100_ssp1 <- rast('data/ssp126/chelsav2/GLOBAL/climatologies/2071-2100/IPSL-CM6A-LR/ssp126/bio/CHELSA_bio1_2071-2100_ipsl-cm6a-lr_ssp126_V.2.1.tif')
-temp_2100_ssp1 <- crop(temp_2100_ssp1,europe)
-
-
-temp_2100_ssp5 <- rast('data/ssp585/chelsav2/GLOBAL/climatologies/2071-2100/IPSL-CM6A-LR/ssp585/bio/CHELSA_bio1_2071-2100_ipsl-cm6a-lr_ssp585_V.2.1.tif')
-temp_2100_ssp5 <- crop(temp_2100_ssp5,europe)
-
-temp_2015
-elev_test <- aggregate(elev,fact = 15)
-temp_2015_test<- aggregate(temp_2015,fact = 30)
-prec_2015_test<- aggregate(prec_2015,fact = 30)
-
-max <- which.max(landuse_2015)
-max <- project(max,elev_test)
-max <- as.factor(max)
-temp_2015_test <- project(temp_2015_test,elev_test)
-prec_2015_test <- project(prec_2015_test,elev_test)
-
-stack <- c(max,elev_test,temp_2015_test,prec_2015_test)
-names(stack) <- c('landuse','elevation','temperature','precipitation')
-writeRaster(stack, "session1_stack.tif", overwrite = TRUE)
 
 
 
